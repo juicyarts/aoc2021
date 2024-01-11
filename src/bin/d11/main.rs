4c54c6666f3526843ab11fd3925cc2f0631bdde1
huess <yildiz.h@eppendorf.de>
2024-01-12 00:05:17 +0100

    feat(day11) pt 1 fin

--- src/bin/d11/main.rs
+++ src/bin/d11/main.rs
@@ -1,9 +1,21 @@
 extern crate aoc2021;
-use std::{collections::HashMap, env, i32};
+use std::{env, i32};
 
+#[rustfmt::skip]
 use crate::utils::read_lines::read_lines;
 use aoc2021::utils;
 use colored::*;
+
+const DIRS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,100 +30,63 @@ fn main() {
     println!("Part 2 {}", p2);
 }
 
-fn d11_p1(source: &str, steps: i32) -> (i32, i64) {
+fn d11_p1(source: &str, steps: i32) -> (usize, i64) {
     let mut l: Vec<Vec<i32>> = Vec::new();
+    let mut rc = 0;
+    let mut cc = 0;
 
     if let Ok(lines) = read_lines(source) {
         lines.flatten().for_each(|row| {
-            let r = row.chars().map(|c| c as i32 - '0' as i32).collect();
+            rc += 1;
+            let r = row
+                .chars()
+                .map(|c| {
+                    if rc == 1 {
+                        cc += 1;
+                    }
+                    return c as i32 - '0' as i32;
+                })
+                .collect();
             l.push(r);
         });
     }
-
-    let count = sim(l, steps);
+    let count = sim(l, steps, rc, cc);
 
     (count, 0)
 }
 
-fn sim(m: Vec<Vec<i32>>, steps: i32) -> i32 {
-    let mut flash_count = 0;
-    let mut fin: Vec<Vec<i32>> = m;
+fn sim(mut m: Vec<Vec<i32>>, steps: i32, r: i32, c: i32) -> usize {
+    (0..steps).fold(0, |acc, _| {
+        m.iter_mut()
+            .for_each(|row| row.iter_mut().for_each(|cell| *cell += 1));
+        let ac = acc
+            + (0..r)
+                .flat_map(|y| (0..c).map(move |x| (x, y)))
+                .fold(0, |acc, (x, y)| {
+                    acc + (m[y as usize][x as usize] > 9)
+                        .then(|| flash(&mut m, (x as usize, y as usize)))
+                        .unwrap_or(0)
+                });
 
-    println!("BEFORE -----------------");
-    print_map(&fin);
-    println!("-------------");
+        print_map(&m);
 
-    for s in 0..steps {
-        let mut nm: Vec<Vec<i32>> = fin
-            .iter()
-            .map(|row| row.iter().map(|c| *c + 1).collect::<Vec<_>>())
-            .collect();
-        for (i, row) in nm.clone().iter().enumerate() {
-            for (j, col) in row.iter().enumerate() {
-                if *col >= 10 {
-                    let (c, nmm) = flash(&vec![i as i32, j as i32], nm);
-                    nm = nmm;
-                    flash_count += c;
-                }
-            }
-        }
-
-        fin = nm
-            .iter()
-            .map(|row| {
-                row.iter()
-                    .map(|c| if *c == -1 { 0 } else { *c })
-                    .collect::<Vec<i32>>()
-            })
-            .collect();
-
-        println!();
-        println!("AFTER STEP {:?}", s);
-        print_map(&fin);
-    }
-
-    flash_count
+        ac
+    })
 }
 
-fn flash(p: &Vec<i32>, mut m: Vec<Vec<i32>>) -> (i32, Vec<Vec<i32>>) {
-    m[p[0] as usize][p[1] as usize] = -1;
-    let mut count = 1;
-
-    let dirs: Vec<(i32, i32)> = vec![
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 0),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
-
-    for dir in dirs {
-        let next = vec![p[0] + dir.0, p[1] + dir.1];
-        let isib = (next[0] >= 0
-            && next[0] < (m.len() as i32)
-            && next[1] >= 0
-            && next[1] < (m[p[0] as usize].len() as i32)
-            && m[next[0] as usize][next[1] as usize] != -1);
-
-        if !isib {
-            continue;
-        }
-
-        m[next[0] as usize][next[1] as usize] += 1;
-
-        if m[next[0] as usize][next[1] as usize] >= 10 {
-            let (c, nm) = flash(&next, m);
-            count += c;
-            m = nm;
-        }
-    }
-
-    //    println!("Round trip ended with {:?}", count);
-    (count, m)
+fn flash(map: &mut Vec<Vec<i32>>, p: (usize, usize)) -> usize {
+    map[p.1][p.0] = 0;
+    DIRS.iter()
+        .map(|(nx, ny)| ((p.0 as isize + nx) as usize, (p.1 as isize + ny) as usize))
+        .fold(1, |acc, (x, y)| {
+            match map.get_mut(y).and_then(|l| l.get_mut(x)) {
+                Some(col) if *col > 0 => {
+                    *col += 1;
+                    acc + (*col > 9).then(|| flash(map, (x, y))).unwrap_or(0)
+                }
+                _ => acc,
+            }
+        })
 }
 
 fn print_map(m: &Vec<Vec<i32>>) {
